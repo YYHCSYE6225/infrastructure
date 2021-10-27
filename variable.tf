021c0ae72dbd0058a5a510e2bce6eec476501a6e
YYhyang <44155580+YYhyang@users.noreply.github.com>
2021-10-27 00:06:34 -0400

    assignment 4

--- variable.tf
+++ variable.tf
@@ -28,3 +28,60 @@ variable "open_ipv6_cidr_block" {
   type        = string
   description = "open cidr block"
 }
+
+variable "bucket_name" {
+  type        = string
+  description = "bucket name"
+}
+
+variable "bucket_acl" {
+  type        = string
+  description = "bucket acl"
+}
+
+variable "lifecycle_name" {
+  type        = string
+  description = "the name and id of lifecycle"
+}
+
+variable "db_parameter_group_name" {
+  type        = string
+  description = "the name of db parameter group"
+}
+
+variable "db_parameter_group_family" {
+  type        = string
+  description = "the family of db parameter group"
+}
+variable "db_allocated_storage" {
+  type        = number
+  description = "the allocated storage of database"
+}
+variable "db_engine" {
+  type        = string
+  description = "the engine of database"
+}
+
+variable "db_engine_version" {
+  type        = string
+  description = "the engine version of database"
+}
+
+variable "db_instance_class" {
+  type = string
+}
+variable "db_name" {
+  type = string
+}
+variable "db_identifier" {
+  type = string
+}
+variable "db_username" {
+  type = string
+}
+variable "db_password" {
+  type = string
+}
+variable "ami_id" {
+  type = string
+}
