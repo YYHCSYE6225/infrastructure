4da2a0718500109c76188814ab79926ca3ace9ca
YYhyang <44155580+YYhyang@users.noreply.github.com>
2021-10-28 00:57:41 -0400

    update

--- s3.tf
+++ s3.tf
@@ -13,6 +13,15 @@ resource "aws_s3_bucket" "s3_bucket" {
       storage_class = "STANDARD_IA"
     }
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
+
   tags = {
     "name" = "yyh.dev.domain.tld"
   }
